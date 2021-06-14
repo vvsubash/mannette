deb6a39fe09cb3cf28b9b7735af9dbdc5c774aab
venkat <subash.venkat007@gmail.com>
2021-06-14 18:39:16 +0530

    added link back to  home for tittle.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="max-w-xl mx-auto">
     <h1 class="text-center text-5xl tracking-tighter font-bold pb-2 pt-4">
+      <nuxt-link to="/" >
       Late Yet Eternal
+      </nuxt-link>
     </h1>
     <hr class="border-4px mx-6 border-black" />
     <Nuxt />
